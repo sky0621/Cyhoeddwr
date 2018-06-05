d1b9abbb2f6546b7cfd28bf3d2f33ee354f0bf1d
sky0621 <sky14work@gmail.com>
2018-06-06 01:24:56 +0900

    continue

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/labstack/echo"
+	"github.com/satori/go.uuid"
 	"go.uber.org/zap"
 )
 
@@ -20,9 +24,31 @@ func main() {
 		text := c.Param("text")
 		logger.Info("/msg/:text", zap.String("text", text))
 
+		uuid, err := uuid.NewV4()
+		if err != nil {
+			panic(err)
+		}
+
+		m := &Message{
+			UUID: uuid.String(),
+			Msg:  text,
+			Ts:   time.Now().UnixNano(),
+		}
+
+		req, err := json.Marshal(m)
+		if err != nil {
+			panic(err)
+		}
 		// FIXME send text to CloudPubSub
+		fmt.Println(req)
 
 		return c.String(http.StatusOK, text)
 	})
 	e.Logger.Fatal(e.Start(":14080"))
+}
+
+type Message struct {
+	UUID string `json:"uuid"`
+	Msg  string `json:"msg"`
+	Ts   int64  `json:"ts"`
 }
