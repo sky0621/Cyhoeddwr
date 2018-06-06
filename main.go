1f3814189f98d68b2ccd051bbd33dfdb209554ed
sky0621 <sky14work@gmail.com>
2018-06-07 01:13:23 +0900

    published to CloudPubSub

--- main.go
+++ main.go
@@ -4,11 +4,18 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
 	"time"
+
+	"google.golang.org/api/option"
+
+	"golang.org/x/net/context"
 
 	"github.com/labstack/echo"
 	"github.com/satori/go.uuid"
 	"go.uber.org/zap"
+
+	"cloud.google.com/go/pubsub"
 )
 
 // 試作版
@@ -19,15 +26,15 @@ func main() {
 	}
 	defer logger.Sync()
 
+	projectID := os.Getenv("PROJECT_ID")
+	credentialsPath := os.Getenv("CREDENTIALS_PATH")
+
 	e := echo.New()
-	e.GET("/msg/:text", func(c echo.Context) error {
+	e.GET("/:text", func(c echo.Context) error {
 		text := c.Param("text")
 		logger.Info("/msg/:text", zap.String("text", text))
 
-		uuid, err := uuid.NewV4()
-		if err != nil {
-			panic(err)
-		}
+		uuid := uuid.NewV4()
 
 		m := &Message{
 			UUID: uuid.String(),
@@ -37,16 +44,47 @@ func main() {
 
 		req, err := json.Marshal(m)
 		if err != nil {
-			panic(err)
+			return c.String(http.StatusInternalServerError, err.Error())
 		}
-		// FIXME send text to CloudPubSub
-		fmt.Println(req)
 
-		return c.String(http.StatusOK, text)
+		fmt.Println(string(req))
+
+		ctx := context.Background()
+		var cli *pubsub.Client
+		if credentialsPath == "" {
+			cli, err = pubsub.NewClient(ctx, projectID)
+		} else {
+			cli, err = pubsub.NewClient(ctx, projectID, option.WithCredentialsFile(credentialsPath))
+		}
+		if err != nil {
+			return c.String(http.StatusInternalServerError, err.Error())
+		}
+		defer func() {
+			if cli != nil {
+				cli.Close()
+			}
+		}()
+
+		tpc := cli.Topic("my-topic-a")
+		res := tpc.Publish(ctx, &pubsub.Message{
+			Attributes: map[string]string{
+				"UUID": uuid.String(),
+			},
+			Data: []byte(text),
+		})
+		serverID, err := res.Get(ctx)
+		if err != nil {
+			return c.String(http.StatusInternalServerError, err.Error())
+		}
+
+		// gcloud alpha pubsub subscriptions pull my-subscription-a
+
+		return c.String(http.StatusOK, serverID)
 	})
 	e.Logger.Fatal(e.Start(":14080"))
 }
 
+// Message ...
 type Message struct {
 	UUID string `json:"uuid"`
 	Msg  string `json:"msg"`
