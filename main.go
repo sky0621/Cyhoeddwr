47e586fa480a8a357dc0aa34c6dd81d5a766672b
sky0621 <sky14work@gmail.com>
2018-06-08 00:46:33 +0900

    continue

--- main.go
+++ main.go
@@ -26,38 +26,55 @@ func main() {
 	}
 	defer logger.Sync()
 
-	projectID := os.Getenv("PROJECT_ID")
-	credentialsPath := os.Getenv("CREDENTIALS_PATH")
+	envProjectID := os.Getenv("PROJECT_ID")
+	envCredentialsPath := os.Getenv("CREDENTIALS_PATH")
+	envAPIKey := os.Getenv("API_KEY")
 
 	e := echo.New()
 	e.GET("/:text", func(c echo.Context) error {
+		uuid := uuid.NewV4().String()
+		logger.Info("START", zap.String("UUID", uuid))
+
+		if envAPIKey != "" {
+			apiKeys := c.Request().Header["API_KEY"]
+			if apiKeys == nil || len(apiKeys) < 1 {
+				logger.Error("No Header[API_KEY]")
+				return response(c, http.StatusUnauthorized)
+			}
+			if envAPIKey != apiKeys[0] {
+				logger.Error("Header[API_KEY] is not match", zap.String("HeaderAPIKey", apiKeys[0]))
+				return response(c, http.StatusUnauthorized)
+			}
+		}
+
 		text := c.Param("text")
 		logger.Info("/msg/:text", zap.String("text", text))
 
-		uuid := uuid.NewV4()
-
 		m := &Message{
-			UUID: uuid.String(),
+			UUID: uuid,
 			Msg:  text,
 			Ts:   time.Now().UnixNano(),
 		}
 
 		req, err := json.Marshal(m)
 		if err != nil {
-			return c.String(http.StatusInternalServerError, err.Error())
+			logger.Error("Error@json.Marshal", zap.String("ERROR", err.Error()))
+			return response(c, http.StatusInternalServerError)
 		}
 
 		fmt.Println(string(req))
 
 		ctx := context.Background()
 		var cli *pubsub.Client
-		if credentialsPath == "" {
-			cli, err = pubsub.NewClient(ctx, projectID)
+		if envCredentialsPath == "" {
+			cli, err = pubsub.NewClient(ctx, envProjectID)
 		} else {
-			cli, err = pubsub.NewClient(ctx, projectID, option.WithCredentialsFile(credentialsPath))
+			cli, err = pubsub.NewClient(ctx, envProjectID, option.WithCredentialsFile(envCredentialsPath))
 		}
 		if err != nil {
-			return c.String(http.StatusInternalServerError, err.Error())
+			logger.Error("Error@json.Marshal", zap.String("ERROR", err.Error()))
+			logger.Error(err.Error())
+			return response(c, http.StatusInternalServerError)
 		}
 		defer func() {
 			if cli != nil {
@@ -68,18 +85,19 @@ func main() {
 		tpc := cli.Topic("my-topic-a")
 		res := tpc.Publish(ctx, &pubsub.Message{
 			Attributes: map[string]string{
-				"UUID": uuid.String(),
+				"UUID": uuid,
 			},
 			Data: []byte(text),
 		})
 		serverID, err := res.Get(ctx)
 		if err != nil {
-			return c.String(http.StatusInternalServerError, err.Error())
+			return response(c, http.StatusInternalServerError)
 		}
 
 		// gcloud alpha pubsub subscriptions pull my-subscription-a
 
-		return c.String(http.StatusOK, serverID)
+		logger.Info("END", zap.String("UUID", uuid), zap.String("serverID", serverID))
+		return response(c, http.StatusOK)
 	})
 	e.Logger.Fatal(e.Start(":14080"))
 }
@@ -89,4 +107,8 @@ type Message struct {
 	UUID string `json:"uuid"`
 	Msg  string `json:"msg"`
 	Ts   int64  `json:"ts"`
+}
+
+func response(c echo.Context, statusCode int) error {
+	return c.String(statusCode, http.StatusText(statusCode))
 }
